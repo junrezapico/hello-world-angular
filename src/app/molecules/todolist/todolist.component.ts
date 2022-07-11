8c5998ecf334e684b2584815f6902a75e09f0262
junrezapico <junre@vananaz.com>
2022-07-11 15:20:59 +0800

    added todo list

--- src/app/molecules/todolist/todolist.component.ts
+++ src/app/molecules/todolist/todolist.component.ts
@@ -4,16 +4,30 @@ import { Todo } from './types';
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.scss']
+  styleUrls: ['./todolist.component.scss'],
 })
 export class TodolistComponent implements OnInit {
-  todo: Todo = {
-    id: 1,
-    todo: 'Windstorm'
-  };
-  constructor() { }
+  todoList: Todo[] = [
+    {
+      id: 1,
+      todo: 'Todo 1',
+    },
+    {
+      id: 2,
+      todo: 'Todo 2',
+    },
+    {
+      id: 3,
+      todo: 'Todo 3',
+    },
+  ];
+  selectedTodo: Todo | undefined = undefined;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+
+  onSelect = (todo: Todo): void => {
+    this.selectedTodo = todo;
   }
-
 }
