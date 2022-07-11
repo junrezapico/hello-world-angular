c6acf191ddc52dca0c5ff6e6ce87c6e593768245
junrezapico <junre@vananaz.com>
2022-07-11 14:47:01 +0800

    added example for input event types

--- src/app/atoms/textinput/textinput.component.ts
+++ src/app/atoms/textinput/textinput.component.ts
@@ -3,30 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-textinput',
   templateUrl: './textinput.component.html',
-  styleUrls: ['./textinput.component.scss']
+  styleUrls: ['./textinput.component.scss'],
 })
 export class TextinputComponent implements OnInit {
   clickMessage = '';
 
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  onMyClickMe = (ev: any) => {
+  onMyClickMe = (ev: MouseEvent) => {
     console.log('MOUSE CLICKED', ev);
     this.clickMessage = 'MOUSE CLICKED';
-  }
+  };
 
-  onMyMouseOver = (ev: any) => {
+  onMyMouseOver = (ev: MouseEvent) => {
     console.log('MOUSE OVERED', ev);
     this.clickMessage = 'MOUSE OVERED';
-  }
+  };
 
-  onChangeKey = (ev: any) => {
+  onChangeKey = (ev: Event) => {
+    if (!(ev.target instanceof HTMLInputElement)) return;
     console.log('CHANGED', ev.target.value);
     this.clickMessage = `${ev.target.value}`;
-  }
-
+  };
 }
