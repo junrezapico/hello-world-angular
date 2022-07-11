90ad250aed8ccb15bf39ab7d0346b8cd411d1cf5
junrezapico <junre@vananaz.com>
2022-07-11 14:36:20 +0800

    added examples for events

--- src/app/atoms/textinput/textinput.component.ts
+++ src/app/atoms/textinput/textinput.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./textinput.component.scss']
 })
 export class TextinputComponent implements OnInit {
+  clickMessage = '';
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onMyClickMe = (ev: any) => {
+    console.log('MOUSE CLICKED', ev);
+    this.clickMessage = 'MOUSE CLICKED';
+  }
+
+  onMyMouseOver = (ev: any) => {
+    console.log('MOUSE OVERED', ev);
+    this.clickMessage = 'MOUSE OVERED';
+  }
+
+  onChangeKey = (ev: any) => {
+    console.log('CHANGED', ev.target.value);
+    this.clickMessage = `${ev.target.value}`;
   }
 
 }
