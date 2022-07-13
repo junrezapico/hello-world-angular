d1de74ea9bed66a1ac912104ca693ce39bdfa02e
junrezapico <junre@vananaz.com>
2022-07-13 10:57:25 +0800

    added rerouting logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  email = '';
+  password = '';
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  onChangeKey = (key: 'email' | 'password', ev: Event) => {
+    if (!(ev.target instanceof HTMLInputElement)) return;
+    this[key] = `${ev.target.value}`;
+  };
 
+  handleSubmit = (): void => {
+    if(!this.email || !this.password){
+      alert('missing fields');
+      return;
+    }
+    // this.router.navigate(['todo']);
+    this.router.navigateByUrl('todo');
+  };
 }
